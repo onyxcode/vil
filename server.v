11036516f2d0e43b4fe99f34c9566ae91a889ae5
onyxcode <me@onyxcode.net>
2021-05-03 09:45:21 -0400

    Logging looks nicer

--- server.v
+++ server.v
@@ -1,17 +1,24 @@
 module main
 
 import vweb
+import term
 
 struct App {
     vweb.Context
 }
 
 fn main() {
-    vweb.run<App>(8080)
+    vweb.run<App>(80)
 }
 
 pub fn (mut app App) index() vweb.Result {
-	ip := app.ip()
-	println("IP logged: $ip")
+	if app.ip().starts_with("192.168.") {
+		ip := app.ip() + (" - " + term.bright_green("Local IP"))
+		println("IP logged: " + term.bold(ip))
+	}
+	else {
+		ip := app.ip() + (" - " + term.bright_magenta("Foreign IP"))
+		println("IP logged: " + term.bold(ip))
+	}
 	return $vweb.html()
 }
