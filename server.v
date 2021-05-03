2b992f801591363aa34be1b3c919d4901ac72848
onyxcode <me@onyxcode.net>
2021-05-03 10:20:43 -0400

    Choice for root or non-root ports at startup.

--- server.v
+++ server.v
@@ -2,13 +2,38 @@ module main
 
 import vweb
 import term
+import os
+
+#include <signal.h>
+
+fn handler() {
+	println('')
+	exit(0)
+}
 
 struct App {
     vweb.Context
 }
 
 fn main() {
-    vweb.run<App>(80)
+	os.signal(2, handler)
+	os.signal(1, handler)
+    mut prompt := os.input_opt("Please enter `y` if you are sudo/root, otherwise enter `n`.\n➥ ") or {
+			exit(1)
+			panic('Exiting: $err')
+		}
+	match prompt {
+	"y" {
+		vweb.run<App>(80)
+	}
+	"n" {
+		vweb.run<App>(8080)
+	}
+	else {
+		println("Exiting...")
+	}
+	}
+	exit(0)
 }
 
 pub fn (mut app App) index() vweb.Result {
